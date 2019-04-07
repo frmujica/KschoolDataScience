52baf040e26ad2527e4c0c2f3ff59a3f248c6fbc
Jesús Ricardo Ballesteros Molina <jrballesteros05@gmail.com>
2019-04-07 18:37:17 +0200

    Delete launchrevealjs.sh

--- launchrevealjs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-jupyter nbconvert TFMDraft.ipynb  --to slides --post serve --ServePostProcessor.ip="192.168.56.101" --ServePostProcessor.port=8910 --SlidesExporter.reveal_theme=sky --SlidesExporter.reveal_scroll=False --SlidesExporter.reveal_transition=concave
