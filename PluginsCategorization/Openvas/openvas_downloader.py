02b1aa5b3240f68abff9001070751fcdbb3c85bc
Jesús Ricardo Ballesteros Molina <jesus.ballesteros@a3sec.com>
2019-04-12 12:24:49 +0200

    refactoring code with lint

--- PluginsCategorization/Openvas/openvas_downloader.py
+++ PluginsCategorization/Openvas/openvas_downloader.py
@@ -1,29 +1,34 @@
-import requests
+"""Openvas Downloader"""
 import configparser
 import tarfile
+import requests
 
 def get_openvas_config_values(configfile):
+    """Read the configuration file and return its values"""
     config = configparser.ConfigParser()
     config.read(configfile)
     return config
 
 def get_openvas_values(configfile):
+    """Read the URL and return its base, this function was desinged for APIS"""
     config = get_openvas_config_values(configfile)
     base_url = config['Openvas']['URL']
-    return (base_url)
+    return base_url
 
-def download_file(plugins_output_file,configfile):
+def download_file(plugins_output_file, configfile):
+    """Download the file from the website"""
     url = get_openvas_values(configfile)
-    r = requests.get(url)
-    if r.status_code == 200:
+    response = requests.get(url)
+    if response.status_code == 200:
         print('Response OK, downloading file from openvas web')
-        with open(plugins_output_file, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=128):
-                fd.write(chunk)
+        with open(plugins_output_file, 'wb') as file_directory:
+            for chunk in response.iter_content(chunk_size=128):
+                file_directory.write(chunk)
         print('File download has been completed')
 
-def decompress_plugin_file(dirpath,plugins_file):
-    print('Descompressing file %s',(plugins_file)) # Must be the absolute file path 
+def decompress_plugin_file(dirpath, plugins_file):
+    """Descompress the file"""
+    print('Descompressing file %s' % plugins_file) # Must be the absolute file path
     tar = tarfile.open(plugins_file)
     tar.extractall(path=dirpath)
     tar.close()
--- PluginsCategorization/Openvas/openvas_parser.py
+++ PluginsCategorization/Openvas/openvas_parser.py
@@ -1,83 +1,71 @@
-from openvas_downloader import download_file,decompress_plugin_file
+"""Download plugins from Openvas website and creates a dataset"""
 import datetime
-import os
+#import os
 import re
 import csv
+from openvas_downloader import download_file, decompress_plugin_file
+
 
 
 
 #Regex that extract important fields to make dataset
 
-oid = re.compile(r'script_oid\(\".*?\.(?P<oid>\d+)\"\)')
-solutiontype = re.compile(r'script\_tag\(name\:.*?"solution\_type\"\,.*?value\:.*?"(?P<solution>.*?)\"\)')
-cvss = re.compile(r'name\:.*?\"cvss_base\"\,.*?value\:.*?\"(?P<cvss>.*?)\"')
-scriptfamily = re.compile(r'script\_family\(\"(?P<script_family>.*?)\"\)')
-solution = re.compile(r'script\_tag\(name.*?\"solution\".*?value.*?\:(?P<solution>.*)')
-tag_solution = re.compile(r'tag\_solution.?\=.*?\"(?P<tag_solution>.*)')
-oids = []
-vendorcsv = []
+OID = re.compile(r'script_oid\(\".*?\.(?P<oid>\d+)\"\)')
+CVSS = re.compile(r'name\:.*?\"cvss_base\"\,.*?value\:.*?\"(?P<cvss>.*?)\"')
+FAMILY = re.compile(r'script\_family\(\"(?P<script_family>.*?)\"\)')
+SOLUTION = re.compile(r'script\_tag\(name.*?\"solution\".*?value.*?\:(?P<solution>.*)')
+TAG_SOL = re.compile(r'tag\_solution.?\=.*?\"(?P<tag_solution>.*)')
+OIDS = []
+VENDORCSV = []
 
 
 
 def rename_file(filename):
+    """Rename a filename"""
     filename = filename + '_' + str(datetime.datetime.now().time()) + '.tar.bz2'
     return filename
 
-def read_vendor_reference_csv(fileName):
+def read_vendor_reference_csv(filename):
+    """ Read vendor references from file and saves into list"""
     reference_regex_csv = []
-    with open(fileName,'rb') as VR:
-        reader = csv.reader(VR)
+    with open(filename, 'rb') as vendors:
+        reader = csv.reader(vendors)
         next(reader, None)  # skip the headers
         for row in reader:
             reference_regex_csv.append(row[2])
     return reference_regex_csv
 
-def loof_for_vendor_reference(filename,content):
-    vendor_references = []
-    content = content.upper()
-    for i in filename:
-        i = str(i)
-        vr = re.compile(i)
-        vendor_ref = vr.findall(content)
-        if vendor_ref:
-            for i in vendor_ref:
-                if type(i) is str:
-                    vendor_references.append(i)
-                if type(i) is tuple:
-                    i = list(set(i))
-                    i = filter(None,i)
-                    for l in i:
-                        vendor_references.append(l)
-    vendor_references=' '.join(vendor_references)
-    return vendor_references
-
 def get_block_of_text(start, end, file_object):
-    inRecordingMode = False
+    """Get just the block of text that we need it to create the dataset"""
+    recording_mode = False
     for line in file_object:
-        if not inRecordingMode:
-            if line.decode().replace(' ','').rstrip().startswith(start):
-                inRecordingMode = True
-        elif line.decode().rstrip().replace(' ','').rstrip().startswith(end):
-            inRecordingMode = False
+        if not recording_mode:
+            if line.decode().replace(' ', '').rstrip().startswith(start):
+                recording_mode = True
+        elif line.decode().rstrip().replace(' ', '').rstrip().startswith(end):
+            recording_mode = False
         else:
             yield line
 
 def main():
+    """ Main function """
+    vendor_list = read_vendor_reference_csv('../VendorReferences.csv')
     openvasfile = rename_file('OpenvasPlugins/PluginsOpenvas')
-    openvasdestfile = './' + openvasfile.replace('.tar.bz2',"")
+    openvasdestfile = './' + openvasfile.replace('.tar.bz2', "")
     configs = 'OpenvasConfigs/openvasconfig.ini'
-    download_file(openvasfile,configs)
-    decompress_plugin_file(openvasdestfile,openvasfile)
+    download_file(openvasfile, configs)
+    decompress_plugin_file(openvasdestfile, openvasfile)
+    print(vendor_list)
     # for root, subdirs, files in os.walk(openvasdestfile):
     #     print(subdirs)
-    # #Files walking   
-    #     for f in files: 
+    # #Files walking
+    #     for f in files:
     #         list_file_path = os.path.join(root, f)
     #         with open(list_file_path,'rb') as list_file:
     #             f_content = list_file.read()
-                
+
 main()
-# with open('OpenvasPlugins/OpenvasPlugins/PluginsOpenvas_19:38:20.414675/aas_detect.nasl','rb') as f:
-#     hi = get_block_of_text("if(description)", "include(", f)
-#     for g in hi:
-#         print(g)
+#with open('./OpenvasPlugins/PluginsOpenvas_11:18:41.777440/aas_detect.nasl','rb') as f:
+#    hi = get_block_of_text("if(description)", "include(", f)
+#    for g in hi:
+#        print(g)
