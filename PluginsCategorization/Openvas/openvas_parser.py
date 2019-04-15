05f94bcdf440439f1a39cec4c014a8658ce8c641
Jesús Ricardo Ballesteros Molina <jesus.ballesteros@a3sec.com>
2019-04-15 18:17:57 +0200

    Improved block text extractor and added created_date

--- PluginsCategorization/Openvas/openvas_parser.py
+++ PluginsCategorization/Openvas/openvas_parser.py
@@ -2,6 +2,7 @@
 import os
 import re
 import csv
+import json
 from openvas_downloader import download_file, decompress_plugin_file
 
 #Regex that extract important fields to make dataset
@@ -14,7 +15,7 @@ TAG_SOL = re.compile(r'tag\_solution.?\=.*?\"(?P<tag_solution>.*)')
 SCRIPTCATEGORY = re.compile(r'script\_category\((?P<script_category>.*?)\)')
 SCRIPTNAME = re.compile(r'script\_name\(.*?(\"|\')(?P<script_name>.*?)(\"|\').*?\)')
 SCRIPTNAME_ALTERNATIVE = re.compile(r'name.*?\=.*?(\"|\')(?P<script_name>.*?)(\"|\')')
-OIDS = []
+CREATION_DATE = re.compile(r'creation_date\"\,.*?value\:\"(?P<creation_date>\d+\-\d+\-\d+\s+\d+\:\d+\:\d+)')
 VENDORCSV = []
 
 def read_vendor_reference_csv(filename):
@@ -28,25 +29,6 @@ def read_vendor_reference_csv(filename):
             reference_regex_csv.append(row[2])
     return reference_regex_csv
 
-# def get_block_of_text(start, end, file_object):
-#     """Get just the block of text that we need it to create the dataset"""
-#     recording_mode = False
-#     print(file_object)
-#     for line in file_object:
-#         if not recording_mode:
-#             if line.replace(' ', '').rstrip().startswith(start):
-#                 recording_mode = True
-#         elif line.rstrip().replace(' ', '').rstrip().startswith(end):
-#             recording_mode = False
-#         else:
-#             yield line
-
-def block_of_text_in_clear(blockoftextobject):
-    """Convert block of text object in clear text"""
-    for text in blockoftextobject:
-        print(text)
-        #return text
-
 def look_for_data(filecontent, filename):
     """Function that look for OID first and then look for other fields"""
     plugin_data = "None"
@@ -55,34 +37,43 @@ def look_for_data(filecontent, filename):
         plugin_data = generate_dataset(filecontent, lookforoid.group('oid'), filename)
     return plugin_data
 
-def write_dataset(filename, outputlist):
-    """Write dataset into csv file"""
-    csv_headers = ['oid', 'plugin_name', 'plugin_file', 'script_category', 'text']
-    with open(filename, 'w') as outcsv:
-        writer = csv.writer(outcsv)
-        writer.writerow(csv_headers)
-        #Write data to file
-        for list_object in outputlist:
-            outcsv.write(list_object + "\n")
+def write_dataset_json(filename, outputlist):
+    """Write dataset into json file"""
+    with open(filename, "w") as outjson:
+        for obj in outputlist:
+            outjson.write(json.dumps(obj) + "\n")
+
+def get_block_of_text(start, end, filecontent):
+    """" Function that looks for a block of text """
+    start = start.replace(' ', '').strip()
+    end = end.replace(' ', '').strip()
+    start_index = filecontent.replace(' ', '').strip().index(start)
+    end_index = filecontent.index(end)
+    text = filecontent[start_index + 100:end_index]
+    return text
+
+def clean_text(text):
+    """Clean rubish from text"""
+    text = text.replace('if(description)'.replace(' ', '').strip(), '').replace('{', '')
+    text = text.replace('if(description)', '').replace('{', '')
+    text = re.sub(r'\#+', '', text)
+    text = re.sub(r'^\n+', '', text)
+    text = text.strip()
+    return text
 
 def generate_dataset(filecontent, oid, filename):
     """Function to create "final" dataset"""
     #vendor_reference = "DOES NOT APPLY"
     filecontent = filecontent.decode("ISO-8859-1")
-    start = "if (description)".replace(' ', '').rstrip()
-    end = "exit(0)"
-    start_index = filecontent.replace(' ', '').rstrip().index(start)
-    end_index = filecontent.index(end)
-    text = filecontent[start_index:end_index]
-    text = text.replace('if (description)'.replace(' ', '').strip(), '').replace('{', '')
-    text = text.replace('if (description)', '').replace('{', '')
-    #print("Filename is " + filename)
-    #print(text)
-    #text = re.search(r'^if.\(description\).*^include\(', filecontent, re.MULTILINE | re.DOTALL)
-    #print(text.group(0))
+    text = get_block_of_text("if (description)", "exit(0);", filecontent)
+    text = clean_text(text)
     look_for_script_category = SCRIPTCATEGORY.search(filecontent)
     look_for_script_name = SCRIPTNAME.search(filecontent)
     look_for_script_name_alt = SCRIPTNAME_ALTERNATIVE.search(filecontent)
+    look_for_creation_date = CREATION_DATE.search(filecontent)
+    print(filename)
+    if look_for_creation_date:
+        creation_date = look_for_creation_date.group('creation_date')
     if look_for_script_category:
         script_category = look_for_script_category.group('script_category')
     if look_for_script_name:
@@ -95,8 +86,9 @@ def generate_dataset(filecontent, oid, filename):
         #Missing vendor references
     plugin_name = plugin_name.replace(',', ' ')
     plugin_name = plugin_name.replace('\"', '')
-    csvdata = '\"' + str(oid) + '\"' +  ',' + '\"' + str(plugin_name) + '\"' + ',' + '\"' + str(filename) + '\"' + ',' + '\"' + str(script_category) + '\"' + ',' + '\"' + str(text) + '\"'
-    return csvdata
+    jsondata = {"oid": oid, "creation_date": creation_date, "plugin_name": plugin_name,
+                "filename": filename, "script_category": script_category, "text": text}
+    return jsondata
 
 def walk_directories(directory, outputfile):
     """Function to walk for every single file on specific directory"""
@@ -110,7 +102,7 @@ def walk_directories(directory, outputfile):
             with open(list_file_path, 'rb') as list_file:
                 f_content = list_file.read()
                 oids_list.append(look_for_data(f_content, file))
-    write_dataset(outputfile, oids_list)
+    write_dataset_json(outputfile, oids_list)
 
 def main():
     """ Main function """
@@ -118,21 +110,9 @@ def main():
     openvasfile = 'OpenvasPlugins/PluginsOpenvas.tar.bz2'
     openvasdestfile = openvasfile.replace('.tar.bz2', "")
     configs = 'OpenvasConfigs/OpenvasConfig.ini'
-    ouputcsvfile = 'DatasetInput/OpenvasLookup.csv'
+    ouputcsvfile = 'DatasetInput/OpenvasLookup.json'
     download_file(openvasfile, configs)
     decompress_plugin_file(openvasdestfile, openvasfile)
-    #print(vendor_list)
     walk_directories(openvasdestfile, ouputcsvfile)
-    # for root, subdirs, files in os.walk(openvasdestfile):
-    #     print(subdirs)
-    # #Files walking
-    #     for f in files:
-    #         list_file_path = os.path.join(root, f)
-    #         with open(list_file_path,'rb') as list_file:
-    #             f_content = list_file.read()
 
 main()
-#with open('./OpenvasPlugins/PluginsOpenvas_11:18:41.777440/aas_detect.nasl','rb') as f:
-#    hi = get_block_of_text("if(description)", "include(", f)
-#    for g in hi:
-#        print(g)
